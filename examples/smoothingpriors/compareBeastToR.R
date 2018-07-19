a99116c2ef4c6ba5b5d201773af13c4d561e8fdf
Louis <louis.duplessis@env.ethz.ch>
2018-07-19 17:09:51 +0100

    Changes to previous tests file

--- examples/smoothingpriors/compareBeastToR.R
+++ examples/smoothingpriors/compareBeastToR.R
@@ -35,11 +35,11 @@ lines(t,sample_hpd[3,],col=pal.dark(cgreen),lty=2,lwd=2)
 
 # Simulate OU-trajectory and get HPD (blue and red)
 t     <- seq(0,9,by=1)
-x0_p  <- getPrior(runif,1,min=20,max=22)
-mu    <- 20
+x0_p  <- getPrior(rexp,1,rate=10)
+mu    <- 3
 sigma <- 1
 nu    <- 0.1
-X     <- plotOUProcessHPDEmpirical(x0_p, t, mu, sigma, nu, ylim = c(15,25), main='ouprior_test2.xml')
+X     <- plotOUProcessHPDEmpirical(x0_p, t, mu, sigma, nu, ylim = c(-4,6), main='ouprior_test2.xml')
 sim_hpd <- getMatrixHPD(X)
 
 # Sample from posterior in BEAST2 and get HPD (orange and green)
